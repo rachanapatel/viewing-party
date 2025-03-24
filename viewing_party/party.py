c8201c25fa973d097017e199343ddc723324cebf
rachanapatel <rmp226@scarletmail.rutgers.edu>
2025-03-24 14:25:49 -0400

    added wave 1 functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,7 +3,34 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {"title": title, "genre": genre, "rating": rating}
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def watch_movie(user_data, title):
+    for movie_dict in user_data["watchlist"]:
+        print(movie_dict)
+        if movie_dict["title"] == title:
+            user_data["watchlist"].remove(movie_dict)
+            user_data["watched"].append(movie_dict)
+            break
+        
+
+    # if title in user_data["watchlist"]
+
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -23,5 +50,5 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
-def chat():
-    print("Hello!!")
+# def chat():
+#     print("Hello!!")
